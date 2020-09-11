b70c13eb04c4249ceac37469b80182b8acc4cb7c
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2020-09-11 21:23:48 +0700

    add done_at field

--- task/models.py
+++ task/models.py
@@ -3,3 +3,4 @@ from django.db import models
 class Task(models.Model):
   name = models.CharField(max_length=255)
   description = models.TextField()
+  done_at = models.DateTimeField(null=True, blank=True)
